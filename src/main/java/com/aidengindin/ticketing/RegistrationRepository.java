55e95e72b305e55c8e7f52ece9f0cc8f37fe30ff
Aiden Gindin <aiden@thegindins.com>
2024-04-24 21:06:36 -0400

    make RegistrationRepository::findByEventId work
    spring can't figure out how to make it work automatically; I had to attach a @Query annotation and specify the query manually.

--- src/main/java/com/aidengindin/ticketing/RegistrationRepository.java
+++ src/main/java/com/aidengindin/ticketing/RegistrationRepository.java
@@ -3,9 +3,11 @@ package com.aidengindin.ticketing;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
 
+    @Query(value = "SELECT r.* FROM registrations r INNER JOIN products p ON r.product_id = p.id WHERE p.event_id = ?1", nativeQuery = true)
     public List<Registration> findbyEventId(int eventId);
 
 }
